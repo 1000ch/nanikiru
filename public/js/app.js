08c629c91c1fa519d53f39a05163c5cbc8544e44
1000ch <shogo.sensui@gmail.com>
2015-04-20 13:31:33 +0900

    update

--- public/js/app.js
+++ public/js/app.js
@@ -18,11 +18,13 @@ $(function () {
   var $characterTiles = $('.character-tiles').find('li');
   var $bambooTiles = $('.bamboo-tiles').find('li');
   var $dotTiles = $('.dot-tiles').find('li');
+  var $honorTiles = $('.honor-tiles').find('li');
 
   $selectedTiles.on('click', 'li', onSelectedClick);
   $characterTiles.on('click', onTileClick);
   $bambooTiles.on('click', onTileClick);
   $dotTiles.on('click', onTileClick);
+  $honorTiles.on('click', onTileClick)
 
   var selectedList = [];
 
