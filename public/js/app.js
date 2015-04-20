4d30b2fc8806adcddbe2781abca628fa2a79c9ee
1000ch <shogo.sensui@gmail.com>
2015-04-20 14:06:56 +0900

    update

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@ var SelectedListView = React.createClass({
     return React.createElement('ul', {}, this.props.items.map(function (item) {
       return React.createElement('li', {
         'data-kind': item.kind,
+        'data-number': item.number,
         'data-order': item.order
       }, React.createElement('img', {
           src: item.src
@@ -29,9 +30,10 @@ $(function () {
   var selectedList = [];
 
   function onSelectedClick(e) {
-    var kind = this.getAttribute('data-kind');
+    var kind = String(this.getAttribute('data-kind'));
+    var number = Number(this.getAttribute('data-number'));
     selectedList.some(function (item, index, array) {
-      if (kind === item.kind) {
+      if (kind === item.kind && number === item.number) {
         selectedList.splice(index, 1);
         return true;
       }
@@ -53,6 +55,7 @@ $(function () {
 
     selectedList.push({
       kind: String(this.getAttribute('data-kind')),
+      number: String(this.getAttribute('data-number')),
       order: Number(this.getAttribute('data-order')),
       src: String(this.querySelector('img').src)
     });
@@ -70,4 +73,35 @@ $(function () {
       selectedTile
     );
   });
+
+  var query = location.search.replace('?', '');
+  query.split('&').forEach(function (keyValue) {
+    var kv = keyValue.split('=');
+    if (kv.length === 2) {
+      var kind = kv[0];
+      var values = kv[1].split('');
+      switch(kind) {
+        case 'character':
+          values.forEach(function (value) {
+            $characterTiles.filter('[data-number=' + value + ']').click();
+          });
+          break;
+        case 'dot':
+          values.forEach(function (value) {
+            $dotTiles.filter('[data-number=' + value + ']').click();
+          });
+          break;
+        case 'bamboo':
+          values.forEach(function (value) {
+            $bambooTiles.filter('[data-number=' + value + ']').click();
+          });
+          break;
+        case 'honor':
+          values.forEach(function (value) {
+            $honorTiles.filter('[data-number=' + value + ']').click();
+          });
+          break;
+      }
+    }
+  })
 });
